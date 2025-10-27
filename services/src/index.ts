dcf32b115125dc8f64c48020c284f1d06d718bb9
Ben Nabet <nabetbenjamin@gmail.com>
2025-10-27 19:24:31 +0200

    Revert index.ts from api folder to src folder back and revert vercel.json file to include only outputdirectory

--- services/src/index.ts
+++ services/src/index.ts
@@ -3,11 +3,11 @@ import express from 'express';
 import dotenv from 'dotenv';
 import morgan from 'morgan';
 import swaggerUi from 'swagger-ui-express';
-import { swaggerSpec } from '../swagger';
-import logger from '../logger';
-import healthRouter from '../query-service/routes/health';
-import { queryRouter } from '../query-service/routes/query.routes';
-import imageRouter from '../image-service/routes/image.route';
+import { swaggerSpec } from './swagger';
+import logger from './logger';
+import healthRouter from './query-service/routes/health';
+import { queryRouter } from './query-service/routes/query.routes';
+import imageRouter from './image-service/routes/image.route';
 
 dotenv.config();
 
@@ -28,7 +28,7 @@ app.use('/health', healthRouter);
 app.use('/', queryRouter);
 app.use('/', imageRouter);
 
-const PORT = process.env.PORT ? Number(process.env.PORT) : 3000;
+// const PORT = process.env.PORT ? Number(process.env.PORT) : 3000;
 // app.listen(PORT, () => {
 //   logger.info(`Server running on port ${PORT} - env=${process.env.NODE_ENV || 'development'}`);
 //   logger.info('To run Swagger use: http://localhost:3000/api-docs')
